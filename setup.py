6bcee78d4515917f015b390eadd860364d15f106
Miroff <mr.miroff@gmail.com>
2012-05-25 17:37:09 +0700

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='artpacker',
+      version='1.0',
+      description='Image resources packer to sprite sheets',
+      author='Maksim Gurtovenko',
+      author_email='maksim@gurtovenko.name',
+      url='https://github.com/Miroff/art-packer',
+      packages=['artpacker', 'artpacker.metadata', 'artpacker.packer'],
+      scripts=['bin/art-packer'],
+      )
