8380ffa1a2d818cc14fa349fd5723afae122ef1d
Miroff <mr.miroff@gmail.com>
2012-05-30 02:49:49 +0700

    Added setup.py to dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='artpacker',
-      version='1.0',
+      version='1.0.1',
       description='Image resources packer to sprite sheets',
       author='Maksim Gurtovenko',
       author_email='maksim@gurtovenko.name',
       url='https://github.com/Miroff/art-packer',
       packages=['artpacker', 'artpacker.metadata', 'artpacker.packer', 'artpacker.saver'],
       scripts=['bin/art-packer'],
-      )
+      install_requires=[
+      	'PIL',
+      ],
+    )
